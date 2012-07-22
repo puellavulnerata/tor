d20f9ce73a4139dfea22649e0002e277259b919a
Andrea Shepard <andrea@persephoneslair.org>
2012-07-22 01:50:33 -0700

    Unit tests for new rend_intro_cell_t parser

--- src/test/test.c
+++ src/test/test.c
@@ -1864,6 +1864,7 @@ extern struct testcase_t dir_tests[];
 extern struct testcase_t microdesc_tests[];
 extern struct testcase_t pt_tests[];
 extern struct testcase_t config_tests[];
+extern struct testcase_t introduce_tests[];
 extern struct testcase_t replaycache_tests[];
 
 static struct testgroup_t testgroups[] = {
@@ -1878,6 +1879,7 @@ static struct testgroup_t testgroups[] = {
   { "pt/", pt_tests },
   { "config/", config_tests },
   { "replaycache/", replaycache_tests },
+  { "introduce/", introduce_tests },
   END_OF_GROUPS
 };
 
