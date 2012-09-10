473cd6872d38c96db973c4fae463e7412dbd9e9c
Andrea Shepard <andrea@persephoneslair.org>
2012-09-09 18:13:09 -0700

    squash! Initial channeltls.c/channeltls.h for bug 6465
    Eliminate dead code connection_or_send_destroy() in connection_or.{c,h}

--- src/or/connection_or.h
+++ src/or/connection_or.h
@@ -69,8 +69,6 @@ void connection_or_write_cell_to_buf(const cell_t *cell,
                                      or_connection_t *conn);
 void connection_or_write_var_cell_to_buf(const var_cell_t *cell,
                                          or_connection_t *conn);
-int connection_or_send_destroy(circid_t circ_id, or_connection_t *conn,
-                               int reason);
 int connection_or_send_versions(or_connection_t *conn, int v3_plus);
 int connection_or_send_netinfo(or_connection_t *conn);
 int connection_or_send_certs_cell(or_connection_t *conn);
