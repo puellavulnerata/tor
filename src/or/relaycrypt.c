568acae23a75a73aa5d8c888bd9f7bf6ab73ac7a
Andrea Shepard <andrea@torproject.org>
2012-11-29 18:26:03 -0800

    First draft of parallel relaycrypt data structures

--- src/or/relaycrypt.c
+++ src/or/relaycrypt.c
@@ -12,7 +12,166 @@
 
 #ifdef TOR_USES_THREADED_RELAYCRYPT
 
-#error Threaded relaycrypt is not finished yet; turn it off.
+/* #error Threaded relaycrypt is not finished yet; turn it off. */
+
+/*
+ * This is the master data structure tracking threaded relaycrypt status;
+ * only one should exist per Tor process, and it gets created in
+ * relaycrypt_init() and freed in relaycrypt_free_all().  It has two
+ * lists of tracked objects: a list of active worker thread structures
+ * of type relaycrypt_thread_t and a list of jobs of type
+ * relaycrypt_job_t.
+ */
+
+struct relaycrypt_dispatcher_s {
+  /*
+   * How many worker threads do we want to have?  Use this in
+   * relaycrypt_set_num_workers() to figure out how many to start
+   * or stop.
+   */
+  int num_workers_wanted;
+  /*
+   * List of relaycrypt_thread_t instances; no lock needed since these
+   * are always added and removed in the main thread.
+   */
+  smartlist_t *threads;
+  /*
+   * List of relaycrypt_job_t instances; jobs are added and may have their
+   * status changed by the main thread if it tries to queue a cell to a
+   * (circuit, direction) tuple which does not already have one, and
+   * may have their status modified by a worker thread, or be removed
+   * if the worker thread finishes the job and it has been marked dead
+   * (circuit closed) by the main thread while the worker held it.
+   * Main or worker threads should hold jobs_lock for access to this.
+   * If locking both jobs_lock and the per-job lock in relaycrypt_job_t,
+   * lock this one first so we know we can't deadlock.
+   */
+  smartlist_t *jobs;
+  /*
+   * TODO need jobs_lock mutex - check if we have a portable tor_mutex_t
+   * or equivalent or need to implement one.
+   */
+};
+
+/*
+ * State of a relaycrypt job and cell queues
+ */
+
+struct relaycrypt_job_s {
+  /* TODO mutex for state changes and queue access */
+  /*
+   * Circuit this job is for *outgoing* cells on, or NULL if the circuit
+   * has been closed and this job should go away.  This should be constant
+   * for the lifetime of the job except that the main thread may change it
+   * to NULL once if a circuit dies; since writing a pointer should be atomic
+   * on reasonable machines it'll be safe to check without locking if the
+   * workers want to poll this to see if they should give up and exit early.
+   */
+  circuit_t *circ;
+  /*
+   * State of this job object:
+   *
+   * RELAYCRYPT_JOB_IDLE:
+   *
+   *   No cells are queued to be crypted, but the job object sticks around
+   *   for when some next show up and to hold any crypted cells the main
+   *   thread hasn't seen yet.  The worker field should be NULL and the input
+   *   queue should be empty.
+   *
+   * RELAYCRYPT_JOB_READY:
+   *
+   *   Cells are available on the input queue and this job is eligible for
+   *   dispatch, but hasn't been dispatched yet.  The worker field should be
+   *   NULL and the input queue should be non-empty.
+   *
+   * RELAYCRYPT_JOB_RUNNING:
+   *
+   *   A worker is processing cells on this job; the worker field should point
+   *   to it.
+   *
+   * RELAYCRYPT_JOB_DEAD:
+   *
+   *   A worker finished this and found the circuit field had been set to
+   *   NULL, indicating a dead circuit.  It should be freed at some point.
+   *   The worker field should be NULL.
+   */
+  enum {
+    RELAYCRYPT_JOB_IDLE,
+    RELAYCRYPT_JOB_READY,
+    RELAYCRYPT_JOB_RUNNING,
+    RELAYCRYPT_JOB_DEAD
+  } state;
+  /* If this is in RELAYCRYPT_JOB_RUNNING, what worker has it? */
+  relaycrypt_thread_t *worker;
+  /*
+   * TODO cell queues - simple smartlist at first, maybe, but we probably
+   * want an efficient way for the worker thread to snarf a whole block of
+   * cells at once from the input without having to repeatedly lock/unlock
+   * or hold a lock that the main thread might want while doing CPU-intensive
+   * crypto ops.
+   */
+};
+
+/*
+ * State of a relaycrypt worker
+ */
+
+struct relaycrypt_thread_s {
+  /* TODO lock for worker state access/changes */
+  /*
+   * State of this worker:
+   *
+   * RELAYCRYPT_WORKER_STARTING:
+   *
+   *   The worker was just created and hasn't set its state to IDLE yet;
+   *   the job field should be NULL.
+   *
+   * RELAYCRYPT_WORKER_IDLE:
+   *
+   *   The worker is waiting to be dispatched; the job field should be NULL
+   *
+   * RELAYCRYPT_WORKER_WORKING:
+   *
+   *   The worker is working; the job field should be the relaycrypt_job_t
+   *   it is working on.
+   *
+   * RELAYCRYPT_WORKER_DEAD:
+   *
+   *   The worker has been told to exit and either has or is about to; the
+   *   main thread should join and clean up dead workers at some point.
+   */
+  enum {
+    RELAYCRYPT_WORKER_STARTING,
+    RELAYCRYPT_WORKER_IDLE,
+    RELAYCRYPT_WORKER_WORKING,
+    RELAYCRYPT_WORKER_DEAD
+  };
+  /*
+   * Flag to indicate the worker should be told to exit next time it asks
+   * for more work; this is initially 0 and may be set to 1 once by the main
+   * thread.
+   */
+  unsigned int exit_flag:1;
+  /*
+   * Job the worker is currently working on, if in RELAYCRYPT_WORKER_WORKING
+   */
+  relaycrypt_job_t *working_on;
+  /*
+   * TODO thread object - check if we have a portable tor_thread_t or need to
+   * write one.
+   */
+};
+
+/*
+ * Static relaycrypt function declarations and descriptive comments
+ */
+
+/**
+ * Main loop for relaycrypt worker threads; takes the thread structure
+ * as an argument and returns when the thread exits.
+ */
+
+static void relaycrypt_worker_main(relaycrypt_thread_t *thr);
 
 /* TODO */
 
--- src/or/relaycrypt.h
+++ src/or/relaycrypt.h
@@ -10,7 +10,101 @@
 #define TOR_RELAYCRYPT_H
 #ifdef TOR_USES_THREADED_RELAYCRYPT
 
-/* TODO */
+#include "or.h"
+
+/* The corresponding structs are defined in relaycrypt.c */
+typedef struct relaycrypt_dispatcher_s relaycrypt_dispatcher_t;
+typedef struct relaycrypt_thread_s relaycrypt_thread_t;
+
+/*
+ * Declarations for relaycrypt functions intended to be called  from
+ * the main thread; see relaycrypt.c for static declarations of worker-
+ * thread functions.
+ */
+
+/**
+ * This is intended to be called by the main thread to give a cell to
+ * the relaycrypt workers.  It adds (circuit_t, direction) to the
+ * work queue if it is not already present, and will either queue it
+ * or wake up a worker if one is idle.  The main thread will
+ * receive the crypted cell back in TODO.
+ */
+
+void relaycrypt_queue_cell(circuit_t *circ, cell_direction_t dir,
+                           cell_t *cell);
+
+/**
+ * Call this from the main thread periodically or when notified to handle
+ * crypted cells.  It iterates through all relaydispatcher_job_t instances
+ * with available output and moves the cells onto the corresponding
+ * circuit's output queue.  This corresponds to the second half (after
+ * relay_crypt()) part of the old circuit_receive_relay_cell().
+ *
+ * TODO how does this get called promptly when output becomes available?
+ */
+
+void relaycrypt_handle_output(void);
+
+/**
+ * Alternately, call this to handle crypted cells on just one (circuit,
+ * direction) tuple.
+ */
+
+void relaycrypt_handle_one_circuit(circuit_t *circ, cell_direction_t dir);
+
+/*
+ * Initialization/management functions for the main thread to call
+ */
+
+/**
+ * Call this at startup to initialize relaycrypt; note that this does not
+ * start any worker threads, so you should use relaycrypt_set_num_workers()
+ * after this.
+ */
+
+void relaycrypt_init(void);
+
+/**
+ * Call this to shut down all active workers, join them and then free
+ * all relaycrypt data.
+ */
+
+void relaycrypt_free_all(void);
+
+/**
+ * Get the number of worker threads
+ */
+
+int relaycrypt_get_num_workers(void);
+
+/**
+ * Set the number of worker threads; this may start more workers or tell some
+ * to shut down as needed; if it shuts workers down it does not wait for them
+ * to exit before returning, but no more jobs will be dispatched to them.
+ */
+
+void relaycrypt_set_num_workers(int threads);
+
+/**
+ * Get the current job size
+ */
+
+int relaycrypt_get_cells_per_dispatch(void);
+
+/**
+ * Set the job size for all future job dispatches; this controls the maximum
+ * number of cells a worker thread will try to process before returning the
+ * job to the dispatcher and requesting a fresh one.
+ *
+ * This is an important parameter; it controls how often a worker thread
+ * becomes available, and so how long a circuit may have to wait for service
+ * when there are more queued jobs than workers.  Setting it too high may
+ * cause problematic latency, but lower values have more threading overhead.
+ * For now, it'll be a tunable, but... TODO measure speed and set this to
+ * something sane by default at runtime.
+ */
+
+void relaycrypt_set_cells_per_dispatch(int cells);
 
 #endif
 #endif
