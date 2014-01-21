837f5c7db751c2ffa95e5b7c999ac78778bb5c9b
Andrea Shepard <andrea@torproject.org>
2014-01-21 02:54:35 -0800

    Add append_cell_to_circuit_queue() unit test

--- src/test/test.c
+++ src/test/test.c
@@ -1310,6 +1310,7 @@ extern struct testcase_t status_tests[];
 extern struct testcase_t routerset_tests[];
 extern struct testcase_t router_tests[];
 extern struct testcase_t channel_tests[];
+extern struct testcase_t relay_tests[];
 
 static struct testgroup_t testgroups[] = {
   { "", test_array },
@@ -1344,6 +1345,7 @@ static struct testgroup_t testgroups[] = {
   { "routerset/" , routerset_tests },
   { "router/", router_tests },
   { "channel/", channel_tests },
+  { "relay/" , relay_tests },
   END_OF_GROUPS
 };
 
