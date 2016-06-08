98b98b159ecbc65eef3a49316877f73ca577c4e0
Andrea Shepard <andrea@torproject.org>
2016-06-08 19:43:18 +0000

    Add new keypin_journal_pruner_t structure, keypin_add_line_to_pruner() mechanism and calls from keypin_load_journal_impl()

--- src/or/keypin.h
+++ src/or/keypin.h
@@ -25,20 +25,55 @@ int keypin_check_lone_rsa(const uint8_t *rsa_id_digest);
 
 #ifdef KEYPIN_PRIVATE
 
+/* Forward typedefs */
+typedef struct keypin_ent_st keypin_ent_t;
+typedef struct keypin_journal_line_st keypin_journal_line_t;
+
 /**
  * In-memory representation of a key-pinning table entry.
  */
-typedef struct keypin_ent_st {
+struct keypin_ent_st {
   HT_ENTRY(keypin_ent_st) rsamap_node;
   HT_ENTRY(keypin_ent_st) edmap_node;
   /** SHA1 hash of the RSA key */
   uint8_t rsa_id[DIGEST_LEN];
   /** Ed2219 key. */
   uint8_t ed25519_key[DIGEST256_LEN];
-} keypin_ent_t;
+  /** If we're pruning, pointer to the line info in the pruner */
+  keypin_journal_line_t *line_info;
+};
+
+/**
+ * In-memory representation of a keypin journal file, used when pruning
+ * duplicate/conflict entries.
+ */
+
+struct keypin_journal_line_st {
+  /* Next/prev line pointers */
+  keypin_journal_line_t *next, *prev;
+  /* Parsed keypin entry, or NULL for a comment/reserved line? */
+  keypin_ent_t *ent;
+  /* Exact line so we can re-emit it correctly after pruning */
+  char line[];
+};
+
+typedef struct keypin_journal_pruner_s {
+  /*
+   * Number of lines in the pruner, and number pruned for various
+   * reasons.
+   */
+  int nlines;
+  int nlines_pruned_corrupt, nlines_pruned_duplicate, nlines_pruned_conflict;
+  /* Doubly linked list of lines to preserve order and comment/reserved
+   * lines */
+  keypin_journal_line_t *head, *tail;
+  /* TODO hash tables for detecting conflicts and duplicates */
+} keypin_journal_pruner_t;
 
 STATIC keypin_ent_t * keypin_parse_journal_line(const char *cp);
-STATIC int keypin_load_journal_impl(const char *data, size_t size);
+STATIC int keypin_load_journal_impl(const char *data, size_t size,
+                                    keypin_journal_pruner_t *pruner,
+                                    int also_add_to_main_map);
 
 MOCK_DECL(STATIC void, keypin_add_entry_to_map, (keypin_ent_t *ent));
 #endif
